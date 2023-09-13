3c5b20ebde5a3f719c7938c2707eeda74ed1477a
MHendriF <hendrifebriansyah28@gmail.com>
2023-09-13 12:30:54 +0700

    implementasi query parameter

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -2,13 +2,26 @@ import React from 'react';
 import SearchBar from '../components/SearchBar';
 import MovieList from '../components/MovieList';
 import { searchMovies } from '../utils/data';
+import { useSearchParams } from 'react-router-dom';
+
+function SearchPageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+ 
+  const title = searchParams.get('title');
+
+  function changeSearchParams(keyword) {
+    setSearchParams({ title: keyword });
+  }
+
+  return <SearchPage onSearch={changeSearchParams} activeKeyword={title} />;
+}
 
 class SearchPage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      foundedMovies: []
+      foundMovies: props.activeKeyword ? searchMovies(props.activeKeyword) : []
     };
 
     this.onSearch = this.onSearch.bind(this);
@@ -20,17 +33,18 @@ class SearchPage extends React.Component {
         foundedMovies: searchMovies(keyword)
       };
     });
+    this.props.onSearch(keyword);
   }
 
   render() {
     return (
       <section>
         <h2>Search Movie</h2>
-        <SearchBar search={this.onSearch} />
-        <MovieList movies={this.state.foundedMovies} />
+        <SearchBar search={this.onSearch} defaultKeyword={this.props.activeKeyword} />
+        <MovieList movies={this.state.foundMovies} />
       </section>
     );
   }
 }
 
-export default SearchPage;
+export default SearchPageWrapper;
